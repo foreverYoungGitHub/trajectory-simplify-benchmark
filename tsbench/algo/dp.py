362de97b25780a0ea59c41a3e21f450d0c360a83
Yang Liu <yaliu@MLLA3853.magicleap.ds>
2022-05-06 15:55:17 -0400

    add TDTR_IOU & TDTR_2Points

--- tsbench/algo/dp.py
+++ tsbench/algo/dp.py
@@ -4,38 +4,94 @@ import numpy as np
 
 from tsbench.algo import base, cal_dist, ALGO_REGISTRY
 
+
 @ALGO_REGISTRY.register()
-class DouglasPeucker(base.BaseTS):
+class DP(base.BaseTS):
+    """DouglasPeucker"""
+
+    @staticmethod
+    def recursive_simplify(
+        trajectory: np.ndarray,
+        epsilon: float,
+        dist_func: Callable,
+        start: int,
+        end: int,
+    ) -> List[int]:
+        assert start < end, (start, end)
+        if start + 1 == end:
+            return [start, end]
+        indices = []
+        dists = dist_func(trajectory[start : end + 1])
+        peak_index = dists.argmax()
+        if dists[peak_index] > epsilon:
+            peak_index += start + 1
+            indices += DP.recursive_simplify(
+                trajectory,
+                epsilon,
+                dist_func,
+                start,
+                peak_index,
+            )
+            indices += DP.recursive_simplify(
+                trajectory,
+                epsilon,
+                dist_func,
+                peak_index,
+                end,
+            )
+        else:
+            indices += [start, end]
+        return indices
+
     def dist_func(self, trajectory):
         return cal_dist.cacl_PEDs(trajectory)
 
     def simplify_one_trajectory(
         self, trajectory: np.ndarray, epsilon: float
     ) -> np.ndarray:
-        def recursive_simplify(start: int, end: int) -> List[int]:
-            assert start < end, (start, end)
-            if start + 1 == end:
-                return [start, end]
-            indices = []
-            dists = self.dist_func(trajectory[start : end + 1])
-            peak_index = dists.argmax()
-            if dists[peak_index] > epsilon:
-                peak_index += start + 1
-                indices += recursive_simplify(start, peak_index)
-                indices += recursive_simplify(peak_index, end)
-            else:
-                indices += [start, end]
-            return indices
-
-        indices = np.unique(recursive_simplify(0, len(trajectory) - 1))
+        indices = np.unique(
+            DP.recursive_simplify(
+                trajectory, epsilon, self.dist_func, 0, len(trajectory) - 1
+            )
+        )
         simplified_trajectory = trajectory[indices]
         return simplified_trajectory
 
 
 @ALGO_REGISTRY.register()
-class TDTR(DouglasPeucker):
+class TDTR(DP):
+    """Top Down Time Ratio"""
+
     def dist_func(self, trajectory):
         return cal_dist.cacl_SEDs(trajectory)
+
+
+@ALGO_REGISTRY.register()
+class TDTR_IOU(TDTR):
+    """Top Down Time Ratio with IOU distance"""
+
+    def dist_func(self, trajectory):
+        return cal_dist.cacl_SIOUs(trajectory)
+
+
+@ALGO_REGISTRY.register()
+class TDTR_2Points(TDTR):
+    """Top Down Time Ratio with IOU distance"""
+
+    def simplify_one_trajectory(
+        self, trajectory: np.ndarray, epsilon: float
+    ) -> np.ndarray:
+        lt = trajectory[:, :3]
+        indices = DP.recursive_simplify(
+            lt, epsilon, self.dist_func, 0, len(trajectory) - 1
+        )
+        rb = np.concatenate((trajectory[:, :1], trajectory[:, 3:]), axis=-1)
+        indices += DP.recursive_simplify(
+            rb, epsilon, self.dist_func, 0, len(trajectory) - 1
+        )
+        indices = np.unique(indices)
+        simplified_trajectory = trajectory[indices]
+        return simplified_trajectory
 
 
 # def douglas_peucker(trajectory: np.ndarray, epsilon: float, dist_func: Callable) -> np.ndarray:
