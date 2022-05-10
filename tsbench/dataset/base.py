a44dfdcb96706db2d6498b40af95bdbe2fd739a6
Yang Liu <yaliu@MLLA3853.magicleap.ds>
2022-05-09 22:28:03 -0400

    finish the mot dataset and sample dataset

--- tsbench/dataset/base.py
+++ tsbench/dataset/base.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict, Any
+import pathlib
 
 import numpy as np
 
 
 class BaseDataset(ABC):
+    def __init__(self, path: str) -> None:
+        super().__init__()
+        self.path = pathlib.Path(path)
+
     @abstractmethod
     def convert_dataset_to_trajectories(self, *_: Any, **__: Any) -> List[Dict]:
         """Override this method to update trajectory data"""
--- tsbench/dataset/mot.py
+++ tsbench/dataset/mot.py
@@ -1,4 +1,4 @@
-from sqlite3 import Timestamp
+from pathlib import Path
 from typing import List, Dict, Any
 
 import numpy as np
@@ -8,23 +8,50 @@ from tsbench.dataset import base, DATASET_REGISTRY
 
 @DATASET_REGISTRY.register()
 class MOTDataset(base.BaseDataset):
-    def __init__(self, path: str, **__: Any) -> None:
-        super().__init__()
-
-        self.raw_data = np.loadtxt(path, delimiter=",")
-        self.trajectory_data = self.convert_dataset_to_trajectories()
+    def __init__(
+        self, path: str, max_trajectories_per_file: int = 10000, **__: Any
+    ) -> None:
+        super().__init__(path)
+        self.max_trajectories_per_file = max_trajectories_per_file
+        self.trajectory_data, self.filenames = self.convert_dataset_to_trajectories()
 
     def convert_dataset_to_trajectories(self) -> List[Dict]:
-        ids = np.unique(self.raw_data[:,1])
-        
         trajectories = []
-        for idx in ids:
-            trajectory = self.raw_data[self.raw_data[:, 1]==idx]
-            bbox = trajectory[:, 1:5]
-            bbox[:, 2:] += bbox[:, :2]
-            trajectories.append({
-                "tid": idx,
-                "bbox": bbox,
-                "timestamp": trajectory[:, :1],
-            })
-        return trajectories
+        filenames = []
+        for f_index, txt_file in enumerate(self.path.glob("*.txt")):
+            filenames.append(txt_file.name)
+
+            raw_data = np.loadtxt(txt_file, delimiter=",")
+            ids = np.unique(raw_data[:, 1])
+            for idx in ids:
+                trajectory = raw_data[raw_data[:, 1] == idx]
+                bbox = trajectory[:, 2:6]
+                bbox[:, 2:] += bbox[:, :2]
+                trajectories.append(
+                    {
+                        "tid": f_index * self.max_trajectories_per_file + idx,
+                        "bbox": bbox,
+                        "timestamp": trajectory[:, :1],
+                    }
+                )
+        return trajectories, filenames
+
+    def dump_data(self, output_dir: Path, trajectories: np.ndarray) -> None:
+        all_trajectories = []
+        for d in self.trajectory_data:
+            trajectory = trajectories[d["tid"]]
+            tid = np.ones(trajectory.shape[0]) * d["tid"]
+            trajectory = np.concatenate((tid[:, None], trajectory), axis=1)
+            all_trajectories.append(trajectory)
+        all_trajectories = np.concatenate(all_trajectories, axis=0)
+        all_trajectories[:, [0, 1]] = all_trajectories[:, [1, 0]]
+        all_trajectories[:, 4:] -= all_trajectories[:, 2:4]
+
+        for f_index, filename in enumerate(self.filenames):
+            trajectories = all_trajectories[
+                (all_trajectories[:, 1] // self.max_trajectories_per_file) == f_index
+            ]
+            trajectories[:, 1] = trajectories[:, 1] % self.max_trajectories_per_file
+            np.savetxt(
+                output_dir / filename, trajectories.astype(int), fmt="%i", delimiter=","
+            )
