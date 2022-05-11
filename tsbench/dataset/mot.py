cc47069ed0552fa3684852730264b6965fb4b6be
Yang Liu <yaliu@MLLA3853.magicleap.ds>
2022-05-11 14:57:58 -0400

    fix mot dataset issue

--- tsbench/dataset/mot.py
+++ tsbench/dataset/mot.py
@@ -22,6 +22,7 @@ class MOTDataset(base.BaseDataset):
             filenames.append(txt_file.name)
 
             raw_data = np.loadtxt(txt_file, delimiter=",")
+            raw_data = raw_data[(raw_data[:,7] <= 7)]
             ids = np.unique(raw_data[:, 1])
             for idx in ids:
                 trajectory = raw_data[raw_data[:, 1] == idx]
