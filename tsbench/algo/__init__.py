27ab98a1c0baa966237d226000d3ac1929fdbeb2
Yang Liu <yaliu@MLLA3853.magicleap.ds>
2022-05-23 15:24:44 -0400

    init uniform sample

--- tsbench/algo/__init__.py
+++ tsbench/algo/__init__.py
@@ -9,3 +9,4 @@ It must returns an instance of :class:`BaseTS`.
 from .dp import DP, TDTR, TDTR_IOU, TDTR_2Points, TDTR_Points
 from .dag import DAG, DAG_IOU, DAG_IOUv2, DAG_2Points, DAG_Points
 from .dag_v2 import DAGv2, DAGv2_IOU, DAGv2_2Points, DAGv2_Points
+from .uniform import UniformSample
