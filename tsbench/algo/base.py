1ccf8883bb0dae97993274885c3d4df1ae0880de
yang <liuyoungwork@gmail.com>
2022-05-30 21:42:35 +0800

    update for parallel

--- tsbench/algo/base.py
+++ tsbench/algo/base.py
@@ -4,6 +4,7 @@ from typing import Tuple, Dict
 
 import tqdm
 import numpy as np
+from joblib import Parallel, delayed
 
 
 class BaseTS(ABC):
@@ -23,14 +24,30 @@ class BaseTS(ABC):
         """
         simplified_trajectories = {}
         runtime_per_query = {}
-        for tid, traj in tqdm.tqdm(trajectories.items()):
-            assert np.all(
-                traj[:-1, 0] <= traj[1:, 0]
-            ), "The trajectory is not sorted by time"
+
+        # parallel
+        def parallel_loop(tid, traj):
             ts = time()
             simplified_trajectory = self.simplify_one_trajectory(traj, **params)
-            runtime_per_query[tid] = time() - ts
-            simplified_trajectories[tid] = simplified_trajectory
+            query_runtime = time() - ts
+            return tid, simplified_trajectory, query_runtime
+
+        results = Parallel(n_jobs=4)(
+            delayed(parallel_loop)(tid, traj)
+            for tid, traj in tqdm.tqdm(trajectories.items())
+        )
+        for res in results:
+            simplified_trajectories[res[0]] = res[1]
+            runtime_per_query[res[0]] = res[2]
+
+        # sequential
+        # for tid, traj in tqdm.tqdm(trajectories.items()):
+        #     assert np.all(
+        #         traj[:-1, 0] <= traj[1:, 0]
+        #     ), "The trajectory is not sorted by time"
+        #     ts = time()
+        #     simplified_trajectories[tid] = self.simplify_one_trajectory(traj, **params)
+        #     runtime_per_query[tid] = time() - ts
         return simplified_trajectories, runtime_per_query
 
     @abstractmethod
